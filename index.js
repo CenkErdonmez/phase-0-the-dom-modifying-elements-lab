00842e981cbb787c6b50debb39301a379facf80a
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-13 14:20:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+const rme=document.getElementById("main")
+
+rme.remove()
+
+
+
+// const newHeader = document.createElement("h1")
+
+// newHeader.setAttribute("id","victory") 
+
+
+
+// newHeader.innerHTML="is the champion"
+
+// document.getElementById("victory").innerHTML="cenk is the champion"
+
+// text.innerHTML=("cenk is the champion")
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+// document.body.append(newHeader)
+newHeader.innerHTML = "Cenk is the champion!";
